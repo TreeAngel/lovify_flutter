d3dfe318d9781a43fada2df2272160870c4728c7
TreeAngel <122539511+TreeAngel@users.noreply.github.com>
2024-11-21 09:06:33 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lovify Flutter
+
+An attempt to recreate my old project from native android to flutter, but still android only
---
+++ android/app/src/main/kotlin/com/treeangel/lovify_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.treeangel.lovify_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
